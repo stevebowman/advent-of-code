0476ec0b55b362f838b3e7cd2660a4eb099fd867
Steve Bowman <steve@stevebowman.net>
2017-12-05 10:17:15 +0000

    Convert input to an int array.

--- AdventOfCode/Day5.cs
+++ AdventOfCode/Day5.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace AdventOfCode
 {
@@ -6,7 +8,8 @@ namespace AdventOfCode
     {
         public static string Run()
         {
-            var numbers = File.ReadAllLines(Path.Combine(FileHelper.InputPath, "Day5.txt"));
+            var numbers = File.ReadAllLines(Path.Combine(FileHelper.InputPath, "Day5.txt"))
+                .Select(x => Convert.ToInt32(x)).ToArray();
 
             return "test";
         }
