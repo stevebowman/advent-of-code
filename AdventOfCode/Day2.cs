fd76ce21431cb6ea38f1f4453be62cf63838d6a6
Steve Bowman <hullcitysteve@gmail.com>
2017-12-06 18:42:36 +0000

    Read input for day 2

---
+++ AdventOfCode/Day2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public static class Day2
+    {
+        public static string RunPart1()
+        {
+            var rows = ReadInput();
+
+            return null;
+        }
+
+        public static string RunPart2()
+        {
+            return null;
+        }
+
+        private static List<List<int>> ReadInput()
+        {
+            var rows = new List<List<int>>();
+
+            var lines = File.ReadAllLines(Path.Combine(FileHelper.InputPath, "Day2.txt"));
+            foreach (var line in lines)
+            {
+                const string sep = "\t";
+                rows.Add(line.Split(sep.ToCharArray()).Select(x => Convert.ToInt32(x)).ToList());
+            }
+
+            return rows;
+        }
+    }
+}
