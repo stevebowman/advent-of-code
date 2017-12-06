67708c4c0f4e6bd37c19fddf603a6fb7d9f8e6a9
Steve Bowman <hullcitysteve@gmail.com>
2017-12-06 19:06:36 +0000

    Solution for day 2 part 2

--- AdventOfCode/Day2.cs
+++ AdventOfCode/Day2.cs
@@ -11,17 +11,44 @@ namespace AdventOfCode
         {
             var rows = ReadInput();
 
-            var checkSum = rows.Aggregate(0, (current, row) => current + (row.Max() - row.Min()));
+            var checksum = rows.Aggregate(0, (current, row) => current + (row.Max() - row.Min()));
 
-            return checkSum.ToString();
+            return checksum.ToString();
         }
 
         public static string RunPart2()
         {
-            return null;
+            var rows = ReadInput();
+
+            var checksum = 0;
+            foreach (var row in rows)
+            {
+                var answerFound = false;
+
+                for (var i = 0; i < row.Count; i++)
+                {
+                    for (var j = 0; j < row.Count; j++)
+                    {
+                        if (j == i) continue;
+
+                        if (row[i] % row[j] != 0) continue;
+
+                        checksum = checksum + (row[i] / row[j]);
+                        answerFound = true;
+                        break;
+                    }
+
+                    if (answerFound)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return checksum.ToString();
         }
 
-        private static List<List<int>> ReadInput()
+        private static IEnumerable<List<int>> ReadInput()
         {
             var rows = new List<List<int>>();
 
