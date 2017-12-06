1f69d0e79cca375d1cc7863a97861675d400cd71
Steve Bowman <=>
2017-12-06 09:39:33 +0000

    Added timing information to output

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Runner
 {
@@ -24,11 +25,17 @@ namespace Runner
                     {
                         var type = Type.GetType($"AdventOfCode.Day{dayNumber},AdventOfCode", true);
 
+                        var sw = new Stopwatch();
+                        sw.Start();
                         var result1 = type.GetMethod("RunPart1").Invoke(null, null);
-                        Console.WriteLine($"Day {dayNumber} Part 1: {result1}");
+                        sw.Stop();
+                        Console.WriteLine($"Day {dayNumber} Part 1: {result1} [took {sw.ElapsedMilliseconds} ms]");
 
+                        sw.Reset();
+                        sw.Start();
                         var result2 = type.GetMethod("RunPart2").Invoke(null, null);
-                        Console.WriteLine($"Day {dayNumber} Part 2: {result2}");
+                        sw.Stop();
+                        Console.WriteLine($"Day {dayNumber} Part 2: {result2} [took {sw.ElapsedMilliseconds} ms]");
 
                         Console.WriteLine();
                     }
