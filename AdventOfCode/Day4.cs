b71ef92d7b1895b179a574d4e5c61bee0e24cd0c
Steve Bowman <hullcitysteve@gmail.com>
2017-12-06 22:14:21 +0000

    Solution to day 4 part 1

---
+++ AdventOfCode/Day4.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public static class Day4
+    {
+        public static string RunPart1()
+        {
+            var phrases = ReadInput();
+
+            var validPhrases = phrases.Count(phrase => phrase.Count == phrase.Distinct().Count());
+
+            return validPhrases.ToString();
+        }
+
+        public static string RunPart2()
+        {
+            return null;
+        }
+
+        private static List<List<string>> ReadInput()
+        {
+            var lines = File.ReadAllLines(Path.Combine(FileHelper.InputPath, "Day4.txt"));
+            return lines.Select(line => line.Split(' ').ToList()).ToList();
+        }
+    }
+}
